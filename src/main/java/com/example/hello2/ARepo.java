7706a720f6cf6b89b99a5c5423d1b8c225e84796
gouravSakshath <gouravbisht786@gmail.com>
2024-12-04 12:30:37 +0530

    first commit

---
+++ src/main/java/com/example/hello2/ARepo.java
@@ -0,0 +1,7 @@
+package com.example.hello2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ARepo extends JpaRepository<A,Integer> {
+
+}
---
+++ src/main/java/com/example/hello2/DemoController.java
@@ -0,0 +1,35 @@
+package com.example.hello2;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker")
+public class DemoController {
+	
+	@Autowired
+	ARepo a;
+	
+	@GetMapping("/get")
+	public A testMethod() {
+		
+		Optional<A> byId = a.findById(1);
+		
+		System.out.println(byId.get());
+		return byId.get();
+	}
+	
+	@GetMapping("/test")
+	public String testMethod1() {
+		
+		
+		return "hello";
+	}
+	
+	
+
+}
